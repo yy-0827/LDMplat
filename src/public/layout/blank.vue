919c8e0b23b2e469efa0351c99734a3bd600392f
unknown <yao_ying0827@163.com>
2021-06-10 09:33:38 +0800

    6/9

--- src/public/layout/blank.vue
+++ src/public/layout/blank.vue
@@ -1,18 +1,7 @@
 <template>
     <div class="blank-page">
         <h3>定位跟踪设备管理平台</h3>
-        <!-- <img :src="backImg" width="78" height="78" alt="logo"> -->
         <router-view />
+
     </div>
 </template>
-
-<script>
-//import backImg from "@/assets/img/login-bg.jpg"
-export default {
-    // data(){
-    //     return{
-    //         backImg
-    //     }
-    // }
-}
-</script>
